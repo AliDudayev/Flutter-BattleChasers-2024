44286243579497159136a07de9bd57f88ec8a122
Magomed-Ali Dudayev <nexes.prime@hotmail.com>
2024-11-07 21:05:20 +0100

    Character movement and animations + player finder

--- Assets/Scripts/MonsterProperties.cs
+++ Assets/Scripts/MonsterProperties.cs
@@ -5,66 +5,66 @@ using UnityEngine.UI;
 
 public class MonsterProperties : MonoBehaviour
 {
-    public float strength = 100f;
-    public float maxHealth = 1000f;
-    private float health;
-    private float stamina = 90f;
+    //public float strength = 100f;
+    //public float maxHealth = 1000f;
+    //private float health;
+    //private float stamina = 90f;
 
-    private Slider slider;
+    //private Slider slider;
 
-    private void Start()
-    {
-        health = maxHealth;
+    //private void Start()
+    //{
+    //    health = maxHealth;
 
-        slider = GetComponentInChildren<Slider>();
+    //    slider = GetComponentInChildren<Slider>();
 
-        slider.maxValue = maxHealth;
-        slider.value = health;
-    }
+    //    slider.maxValue = maxHealth;
+    //    slider.value = health;
+    //}
 
-    public void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            Attack(10);
-        }
-    }
+    //public void Update()
+    //{
+    //    if(Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        Attack(10);
+    //    }
+    //}
 
-    // You can add more properties or methods here, if needed
-    public void ApplyDamage(float damage)
-    {
-        health -= damage;
-        slider.value = health;
+    //// You can add more properties or methods here, if needed
+    //public void ApplyDamage(float damage)
+    //{
+    //    health -= damage;
+    //    slider.value = health;
 
-        if (health <= 0)
-        {
-            Destroy(gameObject);
-        }
-    }
+    //    if (health <= 0)
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
 
-    public void Attack(float staminaCost)
-    {
-        stamina -= staminaCost;
-        ApplyDamage(strength * (staminaCost / 10) * 1.1f);
-        Debug.Log(strength * (staminaCost / 10) * 1.1f);
-    }
+    //public void Attack(float staminaCost)
+    //{
+    //    stamina -= staminaCost;
+    //    ApplyDamage(strength * (staminaCost / 10) * 1.1f);
+    //    Debug.Log(strength * (staminaCost / 10) * 1.1f);
+    //}
 
-    public void Guard()
-    {
-        Debug.Log("Guard");
-    }
+    //public void Guard()
+    //{
+    //    Debug.Log("Guard");
+    //}
 
-    public void GainStamina(float value)
-    {
-        stamina += value;
-        if (stamina > 90)
-        {
-            stamina = 90;
-        }
-    }
+    //public void GainStamina(float value)
+    //{
+    //    stamina += value;
+    //    if (stamina > 90)
+    //    {
+    //        stamina = 90;
+    //    }
+    //}
 
-    public float GetStamina()
-    {
-        return stamina;
-    }
+    //public float GetStamina()
+    //{
+    //    return stamina;
+    //}
 }
--- Assets/Scripts/PlayerFinder.cs
+++ Assets/Scripts/PlayerFinder.cs
@@ -5,21 +5,8 @@ using Vuforia;
 
 public class PlayerFinder : MonoBehaviour
 {
-    // Find object with dinoProperties script
-    //public GameObject Character;
-
-    //void Start()
-    //{
-    //    // Get the GameObject associated with the MonsterProperties component
-    //    MonsterProperties monster = FindObjectOfType<MonsterProperties>();
-    //    if (monster != null)
-    //    {
-    //        Character = monster.gameObject;
-    //    }
-    //}
-
-    private GameObject Character; // To hold the first found active target
-    private ObserverBehaviour[] observerBehaviours;
+    public GameObject Character; 
+    public ObserverBehaviour[] observerBehaviours;
 
     void Start()
     {
@@ -29,7 +16,10 @@ public class PlayerFinder : MonoBehaviour
         // Register a callback for each observer to listen to status changes
         foreach (var observer in observerBehaviours)
         {
-            observer.OnTargetStatusChanged += OnTargetStatusChanged;
+            if(observer.name != "VideoBackground")
+            {
+                observer.OnTargetStatusChanged += OnTargetStatusChanged;
+            }
         }
     }
 
@@ -41,59 +31,61 @@ public class PlayerFinder : MonoBehaviour
             // Set Character to the GameObject of the first detected target if not already set
             if (Character == null)
             {
-                //transform.position = behaviour.transform.position;
-                //transform.rotation = behaviour.transform.rotation;
-
                 Character = behaviour.transform.GetChild(0).gameObject;
 
                 GameObject instantiatedCharacter = Instantiate(Character, transform);
-                instantiatedCharacter.SetActive(true);
-
-                StartCoroutine(SetCharacterPositionAfterDelay(instantiatedCharacter, behaviour, 1f));
-
-                // De skinnedMeshRenderer en canvastonden uit dus ik zet ze aan
-                //SkinnedMeshRenderer skinnedMeshRenderer =  instantiatedCharacter.GetComponentInChildren<SkinnedMeshRenderer>();
-                //skinnedMeshRenderer.enabled = true;
-                //Canvas canvas = instantiatedCharacter.GetComponentInChildren<Canvas>();
-                //canvas.enabled = true;
-            }
-
-            if (Character == null)
-            {
-                //// Instantiate the character at the target's initial position
-                //GameObject instantiatedCharacter = Instantiate(behaviour.transform.GetChild(0).gameObject);
                 //instantiatedCharacter.SetActive(true);
 
-                //// Start a coroutine to wait for 1 second before setting the position and rotation
-                //StartCoroutine(SetCharacterPositionAfterDelay(instantiatedCharacter, behaviour, 1f));
+                //foreach (var renderer in instantiatedCharacter.GetComponentsInChildren<SkinnedMeshRenderer>(true))
+                //{
+                //    renderer.enabled = true;
+                //}
+                //foreach (var canvas in instantiatedCharacter.GetComponentsInChildren<Canvas>(true))
+                //{
+                //    canvas.enabled = true;
+                //}
 
-                //// Store the instantiated character in the Character variable (if needed later)
-                //Character = instantiatedCharacter;
+                // Enable all children and components recursively
+                EnableAllComponents(instantiatedCharacter);
+
+                StartCoroutine(SetCharacterPositionAfterDelay(instantiatedCharacter, behaviour, 1f));
             }
+        }
+    }
+
+    private void EnableAllComponents(GameObject obj)
+    {
+        // Enable the GameObject itself
+        obj.SetActive(true);
+
+        // Loop through all components on this GameObject and enable them if possible
+        foreach (var component in obj.GetComponents<Component>())
+        {
+            if (component is Renderer renderer)
+                renderer.enabled = true;
+            else if (component is Collider collider)
+                collider.enabled = true;
+            else if (component is Canvas canvas)
+                canvas.enabled = true;
+        }
+
+        // Recursively enable all child objects and their components
+        foreach (Transform child in obj.transform)
+        {
+            EnableAllComponents(child.gameObject);
         }
     }
 
     private IEnumerator SetCharacterPositionAfterDelay(GameObject instantiatedCharacter, ObserverBehaviour behaviour, float delay)
     {
-        // Wait for the specified amount of time (1 second in this case)
         yield return new WaitForSeconds(delay);
 
-        // Set the position and rotation after the delay to ensure they are updated
         transform.position = behaviour.transform.position;
         transform.rotation = behaviour.transform.rotation;
 
-        SkinnedMeshRenderer skinnedMeshRenderer = instantiatedCharacter.GetComponentInChildren<SkinnedMeshRenderer>();
-        skinnedMeshRenderer.enabled = true;
-        Canvas canvas = instantiatedCharacter.GetComponentInChildren<Canvas>();
-        canvas.enabled = true;
+        //SkinnedMeshRenderer skinnedMeshRenderer = instantiatedCharacter.GetComponentInChildren<SkinnedMeshRenderer>();
+        //skinnedMeshRenderer.enabled = true;
+        //Canvas canvas = instantiatedCharacter.GetComponentInChildren<Canvas>();
+        //canvas.enabled = true;
     }
-
-    //private void OnDestroy()
-    //{
-    //    // Unregister the event when the script is destroyed
-    //    foreach (var observer in observerBehaviours)
-    //    {
-    //        observer.OnTargetStatusChanged -= OnTargetStatusChanged;
-    //    }
-    //}
 }
