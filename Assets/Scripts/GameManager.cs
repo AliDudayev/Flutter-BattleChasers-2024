167ab6a36828f27852fc6df84078d5ce4f9b8d41
Magomed-Ali Dudayev <nexes.prime@hotmail.com>
2024-11-26 22:40:13 +0100

    Start changed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -44,7 +44,6 @@ public class GameManager : MonoBehaviour
                 observer.OnTargetStatusChanged += OnTargetStatusChanged;
             }
         }
-        StartCoroutine(StartGame());
     }
 
     private void OnTargetStatusChanged(ObserverBehaviour observer, TargetStatus status)
